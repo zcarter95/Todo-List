6fa89218630e36c9c609bd9065e8fb6886acf82f
Zach Carter <carter.peter.zach1@gmail.com>
2025-08-19 15:48:03 -0700

    expand task

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,5 +1,7 @@
+import { ta } from "date-fns/locale";
 import { addToDoItemToProject, addToDoProjectToList,  setCurrentProject} from "..";
 import { format, parseISO } from "date-fns";
+import editImage from "../images/edit_icon.svg"
 export default class UI {
     static displayCurrentProject(project) {
         const heading = document.getElementById("current-project-heading");
@@ -95,16 +97,32 @@ export default class UI {
             console.log(task);
             let taskItem = document.createElement("div");
             taskItem.classList.add("task");
+            taskItem.id = task.id;
             let titleArea = document.createElement("div");
+            titleArea.classList.add("title-area");
             let h3 = document.createElement("h3");
             h3.textContent = task.title;
             titleArea.appendChild(h3);
-            let infoArea = document.createElement("div");
+            let infoArea = document.createElement('div');
+            infoArea.classList.add("info-area");
             let date = document.createElement("p");
             let dateIso = parseISO(task.dueDate);
             let dateFormatted = format(dateIso, "EEEE, MMMM do, yyyy")
             date.textContent = dateFormatted;
             infoArea.appendChild(date);
+            let editIcon = document.createElement("img");
+            editIcon.src = editImage;
+            editIcon.addEventListener("click", () => {
+                console.log("clicky");
+            })
+            infoArea.appendChild(editIcon);
+            titleArea.appendChild(infoArea);
+            const descriptionArea = document.createElement("div");
+            descriptionArea.classList.add("description-area");
+            const description = document.createElement("p");
+            description.textContent = task.description;
+            descriptionArea.appendChild(description);
+            descriptionArea.style.display = "none";
             switch (task.priority) {
                 case "1":
                     taskItem.style.backgroundColor = "#90EE90";
@@ -117,8 +135,18 @@ export default class UI {
                     break;
             };
             taskItem.appendChild(titleArea);
-            taskItem.appendChild(infoArea);
+            taskItem.appendChild(descriptionArea);
             tasksContainer.appendChild(taskItem);
+            taskItem.addEventListener("click", () => {
+                switch (descriptionArea.style.display) {
+                    case ("none"):
+                        descriptionArea.style.display = "block";
+                        break;
+                    case ("block"):
+                        descriptionArea.style.display = "none";
+                        break;
+                }
+            })
         }  
     }
 }
